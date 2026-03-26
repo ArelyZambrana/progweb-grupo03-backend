dfc0581ef4eef9babad5e51a97306f30108ae92f
ashleyfld <cbbe.ashlyalejandra.figueredo.pe@unifranz.edu.bo>
2026-03-26 18:11:15 -0400

    feat(auth): implementar base de usuarios y roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,5 +11,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // 1. Creamos al Administrador
+    \App\Models\User::factory()->create([
+        'name' => 'Admin AlfaWeb',
+        'email' => 'admin@alfaweb.com',
+        'role' => 'admin',
+        'password' => bcrypt('password123'),
+    ]);
+
+    // 2. Creamos al Instructor
+    \App\Models\User::factory()->create([
+        'name' => 'Profesor Juan',
+        'email' => 'instructor@alfaweb.com',
+        'role' => 'instructor',
+        'password' => bcrypt('password123'),
+    ]);
+
+    // 3. Creamos al Estudiante
+    \App\Models\User::factory()->create([
+        'name' => 'Estudiante Maria',
+        'email' => 'estudiante@alfaweb.com',
+        'role' => 'estudiante',
+        'password' => bcrypt('password123'),
+    ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Models\User; // <-- 1. Agregamos el modelo User aquí
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// --- 2. RUTA DE EVIDENCIA PARA POSTMAN (ORM) ---
+Route::get('/api/usuarios', function () {
+    // Usamos el ORM Eloquent para traer los datos limpios
+    $usuarios = User::select('id', 'name', 'email', 'role')->get();
+    
+    return response()->json([
+        'mensaje' => 'Lista de usuarios obtenida con éxito para AlfaWeb',
+        'usuarios' => $usuarios
+    ]);
+});
+// -----------------------------------------------
+
+require __DIR__.'/auth.php';
